c5a84a17936e433c0004016a5ab3735dbc3b2294
Rafael <rafahoyos08@gmail.com>
2021-03-27 15:17:42 -0300

    pymongo+srv

--- Project_V/app.py
+++ Project_V/app.py
@@ -3,17 +3,26 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-#mongo = MongoClient('')
+mongo = MongoClient('mongodb+srv://MainClusterAccess:aFwfefqOdhf3HhE8@maincluster.r3ote.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = mongo['Vendear']
+
+print('Lista')
+for a in db.list_collection_names():
+    print(a)
 
 #--Code Here--
 
 @app.route('/')
-def home():
+def homePage():
     return render_template("pages/index.html")
 
 @app.route('/about')
-def vendor():
+def vendorPage():
     return render_template("pages/about.html")
+
+@app.route('/verify')
+def verifyPage():
+    return render_template("pages/verify.html")
 
 #-------------
 
